120457a0da8058a824a60549e10e4cd37f048fef
Saurabh Jaybhaye <saurabh.jaybhaye@cognitq.com>
2022-10-10 12:48:45 +0530

    hide and show deployment toggle completed

--- ccc/src/components/InputForm.js
+++ ccc/src/components/InputForm.js
@@ -4,6 +4,7 @@ import "./InputForm.css";
 import { v4 as uuidv4 } from "uuid";
 
 export const InputForm = () => {
+  const [show, setShow] = useState(false);
   const [inputFields, setInputFields] = useState([
     {
       id: uuidv4(),
@@ -76,7 +77,11 @@ export const InputForm = () => {
     <div className="topdiv">
       <Nav />
       <div className="nav">
-        <button type="button" className="btn btn-secondary">
+        <button
+          type="button"
+          onClick={() => setShow(!show)}
+          className="btn btn-secondary"
+        >
           DEPLOYMENTS
         </button>{" "}
         <button
@@ -89,233 +94,239 @@ export const InputForm = () => {
       </div>
 
       <form onSubmit={handleSubmit}>
-        {inputFields.map((inputField) => (
-          <div key={inputField.id} className="incontainer">
-            <div className="row g-3">
-              <div className="detailform">
-                <input
-                  type="text"
-                  readOnly
-                  className="form-control-plaintext"
-                  placeholder="Deployment 1"
-                  name="deployment"
-                  value={inputField.deployment}
-                  onChange={(event) => handleChangeInput(inputField.id, event)}
-                />
+        {show ? (
+          <div>
+            {inputFields.map((inputField) => (
+              <div key={inputField.id} className="incontainer">
+                <div className="row g-3">
+                  <div className="detailform">
+                    <input
+                      type="text"
+                      readOnly
+                      className="form-control-plaintext"
+                      placeholder="Deployment 1"
+                      name="deployment"
+                      value={inputField.deployment}
+                      onChange={(event) =>
+                        handleChangeInput(inputField.id, event)
+                      }
+                    />
+                  </div>
+
+                  <div className="closebtn">
+                    <button
+                      type="button"
+                      className="btn btn-outline-primar"
+                      disabled={inputFields.length === 1}
+                      onClick={() => handleRemoveFields(inputField.id)}
+                    >
+                      X
+                    </button>
+                  </div>
+
+                  <div className="lane">
+                    <div className="maininputs">
+                      *<label> Min Pods </label>
+                      <input
+                        type="number"
+                        placeholder="1"
+                        name="minPods"
+                        value={inputField.minPods}
+                        onChange={(event) =>
+                          handleChangeInput(inputField.id, event)
+                        }
+                      />
+                    </div>
+                    <div className="maininputstwo">
+                      <label> Max Pods </label>
+                      <input
+                        type="number"
+                        name="maxPods"
+                        value={inputField.maxPods}
+                        onChange={(event) =>
+                          handleChangeInput(inputField.id, event)
+                        }
+                      />
+                    </div>
+                  </div>
+
+                  <div className="lanetwo">
+                    <div className="maininputs">
+                      *<label> Min RAM </label>
+                      <input
+                        type="number"
+                        placeholder="1"
+                        name="minRam"
+                        value={inputField.minRam}
+                        onChange={(event) =>
+                          handleChangeInput(inputField.id, event)
+                        }
+                      />
+                    </div>
+
+                    <div className="unit">
+                      <select
+                        name="minRamUnit"
+                        id="unit"
+                        onChange={(event) =>
+                          handleChangeInput(inputField.id, event)
+                        }
+                      >
+                        <option value="MiB">MiB</option>
+                        <option value="GiB">GiB</option>
+                      </select>
+                    </div>
+
+                    <div className="max maininputstwo">
+                      <label> Max RAM </label>
+                      <input
+                        type="number"
+                        name="maxRam"
+                        value={inputField.maxRam}
+                        onChange={(event) =>
+                          handleChangeInput(inputField.id, event)
+                        }
+                      />
+                    </div>
+
+                    <div className="unittwo max">
+                      <select
+                        name="maxRamUnit"
+                        id="unit"
+                        onChange={(event) =>
+                          handleChangeInput(inputField.id, event)
+                        }
+                      >
+                        <option value="MiB">MiB</option>
+                        <option value="GiB">GiB</option>
+                      </select>
+                    </div>
+                  </div>
+
+                  <div className="lanetwo">
+                    <div className="maininputs">
+                      *<label> Min vCPU </label>
+                      <input
+                        type="number"
+                        placeholder="1"
+                        name="minVcpu"
+                        value={inputField.minVcpu}
+                        onChange={(event) =>
+                          handleChangeInput(inputField.id, event)
+                        }
+                      />
+                    </div>
+
+                    <div className="unit">
+                      <select
+                        name="minVcpuUnit"
+                        id="unit"
+                        onChange={(event) =>
+                          handleChangeInput(inputField.id, event)
+                        }
+                      >
+                        <option value="MiB">MiB</option>
+                        <option value="GiB">GiB</option>
+                      </select>
+                    </div>
+
+                    <div className="maininputstwo max">
+                      <label> Max vCPU </label>
+                      <input
+                        type="number"
+                        name="maxVcpu"
+                        value={inputField.maxVcpu}
+                        onChange={(event) =>
+                          handleChangeInput(inputField.id, event)
+                        }
+                      />
+                    </div>
+
+                    <div className="unittwo max">
+                      <select
+                        name="maxVcpuUnit"
+                        id="unit"
+                        onChange={(event) =>
+                          handleChangeInput(inputField.id, event)
+                        }
+                      >
+                        <option value="MiB">MiB</option>
+                        <option value="GiB">GiB</option>
+                      </select>
+                    </div>
+                  </div>
+
+                  <div className="lanetwo storage">
+                    <div className="maininput">
+                      <label> Storage </label>
+                      <input
+                        type="number"
+                        name="storage"
+                        value={inputField.storage}
+                        onChange={(event) =>
+                          handleChangeInput(inputField.id, event)
+                        }
+                      />
+                    </div>
+
+                    <div className="storageUnit">
+                      <select
+                        name="storageUnit"
+                        id="unit"
+                        onChange={(event) =>
+                          handleChangeInput(inputField.id, event)
+                        }
+                      >
+                        <option value="MiB">MiB</option>
+                        <option value="GiB">GiB</option>
+                      </select>
+                    </div>
+                  </div>
+
+                  <div className="reset">
+                    <button type="button" className="btn btn-outline-primar">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="30"
+                        height="30"
+                        fill="currentColor"
+                        className="bi bi-bootstrap-reboot"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M1.161 8a6.84 6.84 0 1 0 6.842-6.84.58.58 0 1 1 0-1.16 8 8 0 1 1-6.556 3.412l-.663-.577a.58.58 0 0 1 .227-.997l2.52-.69a.58.58 0 0 1 .728.633l-.332 2.592a.58.58 0 0 1-.956.364l-.643-.56A6.812 6.812 0 0 0 1.16 8z" />
+                        <path d="M6.641 11.671V8.843h1.57l1.498 2.828h1.314L9.377 8.665c.897-.3 1.427-1.106 1.427-2.1 0-1.37-.943-2.246-2.456-2.246H5.5v7.352h1.141zm0-3.75V5.277h1.57c.881 0 1.416.499 1.416 1.32 0 .84-.504 1.324-1.386 1.324h-1.6z" />
+                      </svg>
+                      <br />
+                      Reset
+                    </button>
+                  </div>
+
+                  <div className="reset">
+                    <button
+                      type="button"
+                      className="btn btn-outline-primar"
+                      onClick={handleAddFields}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="30"
+                        height="30"
+                        fill="currentColor"
+                        className="bi bi-check2-circle"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z" />
+                        <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z" />
+                      </svg>
+                      <br />
+                      Done
+                    </button>
+                  </div>
+                </div>
               </div>
-
-              <div className="closebtn">
-                <button
-                  type="button"
-                  className="btn btn-outline-primar"
-                  disabled={inputFields.length === 1}
-                  onClick={() => handleRemoveFields(inputField.id)}
-                >
-                  X
-                </button>
-              </div>
-
-              <div className="lane">
-                <div className="maininputs">
-                  *<label> Min Pods </label>
-                  <input
-                    type="number"
-                    placeholder="1"
-                    name="minPods"
-                    value={inputField.minPods}
-                    onChange={(event) =>
-                      handleChangeInput(inputField.id, event)
-                    }
-                  />
-                </div>
-                <div className="maininputstwo">
-                  <label> Max Pods </label>
-                  <input
-                    type="number"
-                    name="maxPods"
-                    value={inputField.maxPods}
-                    onChange={(event) =>
-                      handleChangeInput(inputField.id, event)
-                    }
-                  />
-                </div>
-              </div>
-
-              <div className="lanetwo">
-                <div className="maininputs">
-                  *<label> Min RAM </label>
-                  <input
-                    type="number"
-                    placeholder="1"
-                    name="minRam"
-                    value={inputField.minRam}
-                    onChange={(event) =>
-                      handleChangeInput(inputField.id, event)
-                    }
-                  />
-                </div>
-
-                <div className="unit">
-                  <select
-                    name="minRamUnit"
-                    id="unit"
-                    onChange={(event) =>
-                      handleChangeInput(inputField.id, event)
-                    }
-                  >
-                    <option value="MiB">MiB</option>
-                    <option value="GiB">GiB</option>
-                  </select>
-                </div>
-
-                <div className="max maininputstwo">
-                  <label> Max RAM </label>
-                  <input
-                    type="number"
-                    name="maxRam"
-                    value={inputField.maxRam}
-                    onChange={(event) =>
-                      handleChangeInput(inputField.id, event)
-                    }
-                  />
-                </div>
-
-                <div className="unittwo max">
-                  <select
-                    name="maxRamUnit"
-                    id="unit"
-                    onChange={(event) =>
-                      handleChangeInput(inputField.id, event)
-                    }
-                  >
-                    <option value="MiB">MiB</option>
-                    <option value="GiB">GiB</option>
-                  </select>
-                </div>
-              </div>
-
-              <div className="lanetwo">
-                <div className="maininputs">
-                  *<label> Min vCPU </label>
-                  <input
-                    type="number"
-                    placeholder="1"
-                    name="minVcpu"
-                    value={inputField.minVcpu}
-                    onChange={(event) =>
-                      handleChangeInput(inputField.id, event)
-                    }
-                  />
-                </div>
-
-                <div className="unit">
-                  <select
-                    name="minVcpuUnit"
-                    id="unit"
-                    onChange={(event) =>
-                      handleChangeInput(inputField.id, event)
-                    }
-                  >
-                    <option value="MiB">MiB</option>
-                    <option value="GiB">GiB</option>
-                  </select>
-                </div>
-
-                <div className="maininputstwo max">
-                  <label> Max vCPU </label>
-                  <input
-                    type="number"
-                    name="maxVcpu"
-                    value={inputField.maxVcpu}
-                    onChange={(event) =>
-                      handleChangeInput(inputField.id, event)
-                    }
-                  />
-                </div>
-
-                <div className="unittwo max">
-                  <select
-                    name="maxVcpuUnit"
-                    id="unit"
-                    onChange={(event) =>
-                      handleChangeInput(inputField.id, event)
-                    }
-                  >
-                    <option value="MiB">MiB</option>
-                    <option value="GiB">GiB</option>
-                  </select>
-                </div>
-              </div>
-
-              <div className="lanetwo storage">
-                <div className="maininput">
-                  <label> Storage </label>
-                  <input
-                    type="number"
-                    name="storage"
-                    value={inputField.storage}
-                    onChange={(event) =>
-                      handleChangeInput(inputField.id, event)
-                    }
-                  />
-                </div>
-
-                <div className="storageUnit">
-                  <select
-                    name="storageUnit"
-                    id="unit"
-                    onChange={(event) =>
-                      handleChangeInput(inputField.id, event)
-                    }
-                  >
-                    <option value="MiB">MiB</option>
-                    <option value="GiB">GiB</option>
-                  </select>
-                </div>
-              </div>
-
-              <div className="reset">
-                <button type="button" className="btn btn-outline-primar">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="30"
-                    height="30"
-                    fill="currentColor"
-                    className="bi bi-bootstrap-reboot"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M1.161 8a6.84 6.84 0 1 0 6.842-6.84.58.58 0 1 1 0-1.16 8 8 0 1 1-6.556 3.412l-.663-.577a.58.58 0 0 1 .227-.997l2.52-.69a.58.58 0 0 1 .728.633l-.332 2.592a.58.58 0 0 1-.956.364l-.643-.56A6.812 6.812 0 0 0 1.16 8z" />
-                    <path d="M6.641 11.671V8.843h1.57l1.498 2.828h1.314L9.377 8.665c.897-.3 1.427-1.106 1.427-2.1 0-1.37-.943-2.246-2.456-2.246H5.5v7.352h1.141zm0-3.75V5.277h1.57c.881 0 1.416.499 1.416 1.32 0 .84-.504 1.324-1.386 1.324h-1.6z" />
-                  </svg>
-                  <br />
-                  Reset
-                </button>
-              </div>
-
-              <div className="reset">
-                <button
-                  type="button"
-                  className="btn btn-outline-primar"
-                  onClick={handleAddFields}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="30"
-                    height="30"
-                    fill="currentColor"
-                    className="bi bi-check2-circle"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z" />
-                    <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z" />
-                  </svg>
-                  <br />
-                  Done
-                </button>
-              </div>
-            </div>
+            ))}
           </div>
-        ))}
+        ) : null}
         <div className="nav send">
           <button
             type="button"
